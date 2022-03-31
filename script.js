e7368731692871ad00b77b312d2234c57229c479
relicayolanda <101093656+relicayolanda@users.noreply.github.com>
2022-03-31 23:26:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,101 @@
-/ Insert angka ke textview
-function insert(num){
-	document.form.textarea.value = document.form.textarea.value + num;
+let prevNumber = ''
+let calculatorOperator = ''
+let currentNumber = '0'
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
 }
 
-// Fungsi Hitung
-function equal(){
-	var hasil = document.form.textarea.value;
-	document.form.textarea.value = eval(hasil);
+
+const inputNumber = (number) => {
+    if (currentNumber === '0') {
+        currentNumber = number
+    }else {
+        currentNumber += number
+    }
+
 }
 
-// Clean
-function clean(){
-	document.form.textarea.value = "";
+const numbers = document.querySelectorAll(".number")
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+
+})
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    if(calculationOperator === '') {
+        prevNumber = currentNumber
+    }
+
+    calculationOperator = operator
+    currentNumber = '0'
 }
 
-// Delete
-function back(){
-	var hasil = document.form.textarea.value;
-	document.form.textarea.value = hasil.substring(0,hasil.length-1);
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click', () => {
+    calculate()
+    updateScreen(currentNumber)
+})
+
+const calculate = () => {
+    let result = ''
+    switch(calculationOperator) {
+        case '+' :
+            result = parseFloat(prevNumber) + parseFloat(currentNumber)
+            break
+        case '-' :
+            result = prevNumber - currentNumber
+            break
+        case '*' :
+            result = prevNumber * currentNumber
+            break
+        case '/' :
+            result = prevNumber / currentNumber
+            break
+        default:
+            break
+    }
+    currentNumber = result
+    calculationOperator = ''
 }
+
+const clearBtn = document.querySelector('.all-clear')
+
+clearBtn.addEventListener('click', () => {
+    clearAll()
+    updateScreen(currentNumber)
+})
+
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
+}
+
+inputDecimal = (dot) => {
+    if(currentNumber.includes('.')) {
+        return
+    }
+    currentNumber += dot
+}
+
+const decimal = document.querySelector('.decimal')
+
+decimal.addEventListener('click', (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
